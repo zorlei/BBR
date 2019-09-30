fb4b3cb7f9b5ef8dbf57f6abe88f75e3e7392617
zorlei <55622080+zorlei@users.noreply.github.com>
2019-09-30 08:24:10 +0800

    Update ok_bbrplus_centos.sh

--- ok_bbrplus_centos.sh
+++ ok_bbrplus_centos.sh
@@ -20,7 +20,7 @@ if [[ -e /appex/bin/serverSpeeder.sh ]]; then
 	rm -f appex.sh
 fi
 echo -e "下载内核..."
-wget https://github.com/cx9208/bbrplus/raw/master/centos7/x86_64/kernel-${kernel_version}.rpm
+wget https://github.com/zorlei/BBR/blob/master/centos7/x86_64/kernel-4.14.129-bbrplus.rpm
 echo -e "安装内核..."
 yum install -y kernel-${kernel_version}.rpm
 
